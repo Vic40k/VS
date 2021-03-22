7bbc9fc8b773d72d64c36e7ad6378b505542dab5
Viktor <uzmk.svv@promet.ru>
2021-03-22 09:14:13 +0300

    Add InfoScreens view model and bulk controller

---
+++ Controllers/InformationScreensController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using VS_CRM.Data;
+using VS_CRM.Model;
+using VS_CRM.Models;
+using VS_CRM.Models.DATA_DB_Model;
+
+namespace VS_CRM.Controllers
+{
+    [ApiController]
+    [Route("api/infoScreens")]
+    public class InformationScreensController : Controller
+    {
+        DATAContext dbDATA;
+        public InformationScreensController(DATAContext context)
+        {
+            dbDATA = context;
+        }
+        [HttpGet]
+        public IEnumerable<ЗаказыРегион> GetStorageInfo()
+        {
+            try
+            {
+                var result = dbDATA.ЗаказыРегион.ToList();
+                return result;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/InformationScreensModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace VS_CRM.Models
+{
+    public partial class InformationScreenViewModel
+    {
+        public int Order { get; set; }
+		public int Region { get; set; }
+		public int Orderquantity { get; set; }
+		public DateTime OrderDate { get; set; }
+		public int ProducedQuantity { get; set; }
+		public DateTime WarehouseDate { get; set; }
+		public int ShipmentQuantity { get; set; }
+		public DateTime ShipmentDate { get; set; }
+		public int StatusId { get; set; }
+		public string Status { get; set; }
+		public string Client { get; set; }
+		public int Reason { get; set; } 
+		public string Coment { get; set; }
+		public DateTime DelayDate { get; set; }
+    }
+}
