faae15175653ce652d9d2f73c4d8141e5cc38074
Viktor <uzmk.svv@promet.ru>
2021-03-22 10:13:00 +0300

    Get Info screens storage auery prototype

--- Controllers/InformationScreensController.cs
+++ Controllers/InformationScreensController.cs
@@ -19,18 +19,43 @@ namespace VS_CRM.Controllers
             dbDATA = context;
         }
         [HttpGet]
-        public IEnumerable<ЗаказыРегион> GetStorageInfo()
+        public IEnumerable<InformationScreenViewModel> GetStorageInfo()
         {
-            try
-            {
-                var result = dbDATA.ЗаказыРегион.ToList();
-                return result;
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            return null;
+            // TODO async
+            var infoScreensWarehouseData = new List<InformationScreenViewModel>();
+            var startDate = DateTime.Now.AddDays(-3).Date.Add(new TimeSpan(0,0,0)); 
+
+            infoScreensWarehouseData = (from zak in dbDATA.ЗаказыРегион
+                                        join reg in dbDATA.ТоварВЗаказеРегион on zak.Регион equals reg.Регион
+                                        join nom in dbDATA.НоменклатураТоваров on reg.IdТовара equals nom.IdТовара
+                                        join grp in dbDATA.СписокПодгрупп on nom.IdПодгруппы equals grp.IdПодгруппы
+                                        where zak.Завод == 44 
+                                        && zak.ДатаКогдаПроставилоПроизводство != null
+                                        && zak.Дата > startDate
+                                        && zak.NSub != null
+                                        && zak.Регион != 1603
+                                        && zak.ДатаКогдаПроставилоПроизводство != null
+                                        // let a = t.L != null ? t.L : t.A
+                                        select new InformationScreenViewModel
+                                        {
+                                            Order = zak.Номер,
+                                            Region = zak.Регион,
+                                            /*
+                                            Orderquantity { get; set; }
+                                            public DateTime OrderDate { get; set; }
+                                            public int ProducedQuantity { get; set; }
+                                            public DateTime WarehouseDate { get; set; }
+                                            public int ShipmentQuantity { get; set; }
+                                            public DateTime ShipmentDate { get; set; }
+                                            public int StatusId { get; set; }
+                                            public string Status { get; set; }
+                                            public string Client { get; set; }
+                                            public int Reason { get; set; }
+                                            public string Coment { get; set; }
+                                            public DateTime DelayDate { get; set; }
+                                                                                    */
+                                        }).ToList();
+            return infoScreensWarehouseData;
         }
     }
 }
