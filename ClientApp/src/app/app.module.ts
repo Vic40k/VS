30344a4fd6b985cd8c6768b7434704865356aa81
Viktor <uzmk.svv@promet.ru>
2021-03-23 14:41:56 +0300

    Add Angular material modules

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     { path: 'infoScreensWarehouse', component: infoScreensWarehouseComponent },
     { path: 'counter', component: CounterComponent },
     { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-], { relativeLinkResolution: 'legacy' })
+], { relativeLinkResolution: 'legacy' }),
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
