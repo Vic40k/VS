614c4d4020bbc8d7c1fdb15839b01c673c9e805a
Viktor <uzmk.svv@promet.ru>
2021-03-26 16:28:13 +0300

    Implement scrol page mechanism

--- ClientApp/src/app/info-screens/infoScreensWarehouse.component.ts
+++ ClientApp/src/app/info-screens/infoScreensWarehouse.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectorRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { max } from 'rxjs/operators';
 import { DataService } from '../data.service';
 
 @Component({
@@ -15,18 +16,27 @@ import { DataService } from '../data.service';
 export class infoScreensWarehouseComponent implements OnInit {
   id: number; // Screen id
   resultQty: number; // Max results on screen
-  rowData: [];
+  dataStorage: [];
+  dataShow: [];
   private subscription: Subscription;
   private querySubscription: Subscription;
   //tableMode: boolean = true; 
   updatePeriod: number = 60; // seconds
   timeLeft: number = this.updatePeriod;
   interval;
+
+  isScrol = true;
+  scrolInterval = 5; // seconds, must be less than 60 
+  maxResultsPerPage = 20;
+  pageCount = 1;
+  pageCounter = 1;
+
   updateTime: Date = new Date();
 
   constructor(private ref: ChangeDetectorRef, private dataService: DataService, private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // Define params from URL
     this.subscription = this.activateRoute.params.subscribe(params => this.id = params['id']);
     this.querySubscription = this.activateRoute.queryParams.subscribe(
       (queryParam: any) => {
@@ -42,9 +52,17 @@ export class infoScreensWarehouseComponent implements OnInit {
     this.dataService.getWarehouseInfo()
       .subscribe((data: []) => 
       { 
-        this.rowData = data; 
+        this.dataStorage = data; 
+        console.log(this.dataStorage);
+        // Count pages if needed
+        if (this.isScrol)
+          this.pageCount = Math.round(this.dataStorage.length / this.maxResultsPerPage) + 1;
+        console.log(this.pageCount);
+        // Fill array to show in first time
+        if (!this.dataShow)
+          this.dataShow = Object.assign([], this.dataStorage);
+        // Update view
         this.ref.detectChanges(); 
-        console.log(this.rowData); 
       });
   }
 
@@ -53,6 +71,16 @@ export class infoScreensWarehouseComponent implements OnInit {
     this.interval = setInterval(() => {
       if(this.timeLeft > 0) {
         this.timeLeft--;
+        if (this.isScrol) {
+          // Time to scrol to next page
+          if (this.timeLeft % this.scrolInterval === 0) {
+            if (this.pageCounter < this.pageCount)
+              this.pageCounter ++;
+            else
+              this.pageCounter = 1;
+            console.log('scrol to page #' + this.pageCounter);
+          }
+        }
       } else {
         this.timeLeft = this.updatePeriod;
         this.loadinfoScreensWarehouse();
@@ -65,6 +93,11 @@ export class infoScreensWarehouseComponent implements OnInit {
   pauseTimer() {
     clearInterval(this.interval);
   }
+
+  currentShow() {
+    console.log('!');
+  }
+
   /*
   // сохранение данных
   save() {
