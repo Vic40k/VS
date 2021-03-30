2dda8a75e77dff7bf6ad7db9031d67286cbcf9d8
Viktor <uzmk.svv@promet.ru>
2021-03-30 15:30:03 +0300

    Divide observers in ui service (wrong access)

--- ClientApp/src/app/services/userInterface.service.ts
+++ ClientApp/src/app/services/userInterface.service.ts
@@ -5,30 +5,33 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable()
 export class UserIntarfaceService {
 
-    private _loaderSource:any = new BehaviorSubject<any>({});
-    public navbar = this._loaderSource.asObservable();
-    public progressbar = this._loaderSource.asObservable();
+    private _navbarSource:any = new BehaviorSubject<any>({});
+    private _progressbarSource:any = new BehaviorSubject<any>({});
+    public navbar = this._navbarSource.asObservable();
+    public progressbar = this._progressbarSource.asObservable();
 
     private progressbarMode:number = 0; 
 
     // ------- Upper main navigation bar
     setNavbarVisible(isNavbarVisible: boolean){
-        this._loaderSource.next(isNavbarVisible);
+        this._navbarSource.next(isNavbarVisible);
     }
 
     // --------- Upper main progress bar
     setProgressbarValue(progressbarValue: number){
-        if (progressbarValue >= 0 && progressbarValue <= 100)
-            this._loaderSource.next(progressbarValue);
+        if (progressbarValue >= 0 && progressbarValue <= 100){
+            this.stopProgressBarLoading();
+            this._progressbarSource.next(progressbarValue);
+        }
     }
 
     setProgressBarNormalLoading(){
         this.progressbarMode = 1
-        this._loaderSource.next(this.progressbarMode);
+        this._progressbarSource.next(this.progressbarMode);
     }
 
     stopProgressBarLoading(){
         this.progressbarMode = 0
-        this._loaderSource.next(this.progressbarMode);
+        this._progressbarSource.next(this.progressbarMode);
     }
 }
