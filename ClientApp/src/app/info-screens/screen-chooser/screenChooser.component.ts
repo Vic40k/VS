afa196ca0480ec92a40a49863e4c02da0325d54a
Viktor <uzmk.svv@promet.ru>
2021-03-31 16:22:11 +0300

    Screen chooser fuctionality prototype demo

--- ClientApp/src/app/info-screens/screen-chooser/screenChooser.component.ts
+++ ClientApp/src/app/info-screens/screen-chooser/screenChooser.component.ts
@@ -13,7 +13,12 @@ export class ScreenChooserComponent {
   faDesktop = faDesktop;
 
   goToWarehouseScreen(warehouseMonitorId: number = 0) {
-    this.router.navigateByUrl('/sklad/0?autoScroll=true&pageToShow=0&pageToShowFrom=0&pageToShowTo=0&scrolInterval=5&updatePeriod=60&resultQty=0');  
+    if (warehouseMonitorId === 0)
+      this.router.navigateByUrl('/sklad/0?autoScroll=true&pageToShow=0&pageToShowFrom=0&pageToShowTo=0&scrolInterval=5&updatePeriod=60&resultQty=0');  
+    else if (warehouseMonitorId === 1)
+      this.router.navigateByUrl('/sklad/0?autoScroll=false&pageToShow=1&pageToShowFrom=0&pageToShowTo=0&scrolInterval=5&updatePeriod=60&resultQty=0');  
+    else if (warehouseMonitorId === 2)
+      this.router.navigateByUrl('/sklad/0?autoScroll=true&pageToShow=0&pageToShowFrom=2&pageToShowTo=0&scrolInterval=10&updatePeriod=60&resultQty=0');  
   }
 }
 
