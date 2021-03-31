22cc72ae594b8e1a754e158e1d7daace136513e5
Viktor <uzmk.svv@promet.ru>
2021-03-31 15:33:26 +0300

    Try fa on screen chooser

--- ClientApp/src/app/info-screens/screen-chooser/screenChooser.component.ts
+++ ClientApp/src/app/info-screens/screen-chooser/screenChooser.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import { faDesktop } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-counter-component',
@@ -9,8 +10,10 @@ import {Router} from '@angular/router';
 export class ScreenChooserComponent {
   constructor(private router: Router) {}
 
+  faDesktop = faDesktop;
+
   goToWarehouseScreen(warehouseMonitorId: number = 0) {
-    this.router.navigateByUrl('/sklad/' + warehouseMonitorId);  
+    this.router.navigateByUrl('/sklad/0?autoScroll=true&pageToShow=0&pageToShowFrom=0&pageToShowTo=0&scrolInterval=5&updatePeriod=60&resultQty=0');  
   }
 }
 
